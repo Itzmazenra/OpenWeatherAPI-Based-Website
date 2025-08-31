d915f36a739ff76223077221dd9697e5581ba898
Itzmazenra <124457314+Itzmazenra@users.noreply.github.com>
2025-08-31 05:51:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  OPENWEATHER_API_KEY: '564e502499eeb93b4ab8a41195ccf3d4',
+  OPENWEATHER_API_KEY: '564e502499eeb93b4ab8a41195ccf3d4', // Yes i didn't forget to delete it
   PORT: process.env.PORT || 3000
 };
+
